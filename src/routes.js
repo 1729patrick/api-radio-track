d7870df8ac0f6076a5295513d3f0892e63c91e36
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-11-23 01:41:04 +0000

    import route

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,7 @@ import SearchController from './app/controllers/SearchController';
 import GenreController from './app/controllers/GenreController';
 import PlaylistController from './app/controllers/PlaylistController';
 
-import ImportController from './app/controllers/ImportController';
-
 const router = Router();
-
-router.get('/import', ImportController.index);
 
 router.get('/stations', StationController.list);
 router.get('/stations/:id', StationController.index);
